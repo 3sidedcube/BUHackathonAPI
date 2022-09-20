2942180d060404271c6713b1bccc50b2ac817c60
Zak Farmer <zak@3sidedcube.com>
2022-09-20 22:10:11 +0100

    Updated to GET endpoints

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Impact\CalculateHouseholdImpactController;
 use App\Http\Controllers\Impact\CalculateTravelImpactController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/impact/food', CalculateFoodImpactController::class)->name('impact.food');
-Route::post('/impact/household', CalculateHouseholdImpactController::class)->name('impact.household');
-Route::post('/impact/travel', CalculateTravelImpactController::class)->name('impact.travel');
+Route::get('/impact/food', CalculateFoodImpactController::class)->name('impact.food');
+Route::get('/impact/household', CalculateHouseholdImpactController::class)->name('impact.household');
+Route::get('/impact/travel', CalculateTravelImpactController::class)->name('impact.travel');
